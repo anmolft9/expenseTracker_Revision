968dea32fd1c7e64d4d7c181b961f0dc69db3213
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-06-28 23:07:46 +1000

    installed the mongoose package>

--- api/server.js
+++ api/server.js
@@ -1,9 +1,17 @@
 import express from "express";
 
 const app = express();
+import cors from "cors";
 
 const PORT = 8000;
 
+//middleware
+app.use(express.json());
+app.use(cors());
+
+///apis
+import userRouter from "./src/routers/userRouter.js";
+app.use("/api/v1/user", userRouter);
 app.use("/", (req, res) => {
   try {
     res.send("<h3>coming soon</h3>");
