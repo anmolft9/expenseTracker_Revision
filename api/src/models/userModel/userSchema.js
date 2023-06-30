6341255159dbd84f7813f47645966ff499344a32
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-06-30 23:01:56 +1000

    added the userSchema >

---
+++ api/src/models/userModel/userSchema.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const usdrSchema = new mongoose.Schema({
+  status: {
+    type: String,
+    default: "inactive",
+  },
+  firstName: {
+    type: String,
+    require: true,
+  },
+  lastName: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+    index: 1,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
